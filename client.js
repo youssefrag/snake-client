69953daaf91d77d66c507e4c62dca31e49bb65a2
Youssef Ragab <ragab.youssef95@gmail.com>
2022-01-19 13:49:13 -0500

    added client.js

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  })
+
+  conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log('you ded cuz you idled')
+  })
+
+  return conn;
+};
+
+module.exports = connect
--- play.js
+++ play.js
@@ -1,19 +1,4 @@
-const net = require("net");
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  })
-
-  conn.setEncoding("utf8");
-
-  conn.on('connect', () => {
-    console.log('you ded cuz you idled')
-  })
-
-  return conn;
-};
+const connect = require("./client")
 
 console.log('Connecting...');
 connect();
