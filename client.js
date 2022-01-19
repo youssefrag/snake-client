3ccd3d6be897e984b032a886ddfbce52bc98c8ca
Youssef Ragab <ragab.youssef95@gmail.com>
2022-01-19 15:36:12 -0500

    added move fucntionality

--- client.js
+++ client.js
@@ -9,6 +9,14 @@ const connect = function () {
   conn.setEncoding("utf8");
 
   conn.on('connect', () => {
+    console.log('Successfully connected to game server')
+    conn.write('Name: YMR')
+    // setTimeout(() => {
+    //   conn.write('Move: up')
+    // }, 5000);
+  })
+
+  conn.on('data', () => {
     console.log('you ded cuz you idled')
   })
 
